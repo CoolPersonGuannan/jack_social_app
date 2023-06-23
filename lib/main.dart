3ae0b3e12ce54fea5c72c13622721727c196853a
SKTran <tskhoa1999@gmail.com>
2023-06-22 19:43:51 -0700

    Test Push from Khoa Instructor

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
       create: (context) => MyAppState(),
       child: MaterialApp(
         title: 'Namer App',
+        debugShowCheckedModeBanner: false,
         theme: ThemeData(
           useMaterial3: true,
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
